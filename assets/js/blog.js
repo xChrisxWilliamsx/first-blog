59981b5c7642ca48aa0dd139d05e7854ebb5c25b
xChrisxWilliamsx <williamsc0325@gmail.com>
2024-06-26 21:54:35 -0700

    Adds post saving and CSS theme changes

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,32 @@
-// handles taking local storage inputs and creates their corresponding posts
+// This file handles retrieving Local Storage and posting blog post
+
+// Retrieves local Storage
+let commentsPost = localStorage.getItem('comments');
+
+// Converts comments back to usable code
+commentsPost = JSON.parse(commentsPost);
+
+// creates post for each object saved to local Storage
+commentsPost.forEach(comment => {
+    
+    //Targets html class to insert html
+    const blogPost = document.querySelector(".posts");
+    
+    // Inserts HTML template
+    blogPost.insertAdjacentHTML("beforeend", `
+        <div class="postBox postBoxLight">
+            <h2 class="postTitle postTitleLight">
+                ${comment.title}
+            </h2>
+            <p class="postContent postContentLight">
+                ${comment.content}
+            </p>
+            <div class="postUser postUserLight">
+                <p>
+                    Posted by: ${comment.name}
+                </p>
+            </div>
+    
+        </div>`
+    );
+});
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,18 +1,36 @@
-// handles saving form to local storage
+// Handles saving form to local storage
 
+// Targets theme button
 const subBtn = document.querySelector('.subContent');
 
 // Allows user input from form to store locally
 subBtn.addEventListener('click', function(event) {
     event.preventDefault();
-    
+
     const titleInput = document.querySelector('#title').value;
     const contentInput = document.querySelector('#content').value;
     const nameInput = document.querySelector('#username').value;
-    
-    localStorage.setItem('title', titleInput);
-    localStorage.setItem('content', contentInput);
-    localStorage.setItem('name', nameInput);
+
+    const newComment = {
+        title: titleInput,
+        content: contentInput,
+        name: nameInput
+    };
+
+    let comments = localStorage.getItem('comments')
+
+    if (comments) {
+        comments = JSON.parse(comments);
+    } else {
+        comments = [];
+    }
+
+    comments.push(newComment);
+
+    comments = JSON.stringify(comments);
+
+    localStorage.setItem('comments', comments);
 
     window.location.href = "blog.html";
-});
+});    
+
